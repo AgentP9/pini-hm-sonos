c08e917949740b6661781cd271fc2b0e3706339f
AgentP9 <spectre-no1@gmx.de>
2026-01-19 17:43:54 +0100

    Improved image-script

--- generate_img.sh
+++ generate_img.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
+
+# Check if VERSION file exists
+if [ ! -f VERSION ]; then
+    echo "ERROR: VERSION file not found"
+    exit 1
+fi
+
+# Read version and validate it's not empty
+VERSION=$(tr -d '\n\r' < VERSION)
+if [ -z "$VERSION" ]; then
+    echo "ERROR: VERSION file is empty"
+    exit 1
+fi
+
+echo "Building sonos2-addon-${VERSION}.tar.gz"
+
 tar=$(which gtar) # OSX gnu tar
-if [ -z $tar ]; then
+if [ -z "$tar" ]; then
     tar="tar"
 fi
 mkdir -p tmp
@@ -15,6 +31,6 @@ cp -a update_script tmp/
 cp -a VERSION tmp/sonos2/
 cd tmp
 
-$tar --owner=root --group=root --exclude=.DS_Store -czvf ../sonos2-addon-$(cat ../VERSION).tar.gz *
+$tar --owner=root --group=root --exclude=.DS_Store -czvf ../sonos2-addon-${VERSION}.tar.gz *
 cd ..
 rm -rf tmp
